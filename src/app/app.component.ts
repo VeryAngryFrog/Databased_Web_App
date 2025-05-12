ed67b775c483144821a29054d76ff7eef4a478eb
TBY01Z <rieckersniclas@gmail.com>
2025-05-12 13:25:41 +0200

    update compo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent} from "./navbar/navbar.component";
 import { LoginComponent } from './login/login.component';
+import {RegistrationComponent} from "./registration/registration.component";
+import {ResetPasswordComponent} from "./reset-password/reset-password.component";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, LoginComponent, NavbarComponent],
+    imports: [RouterOutlet, LoginComponent, NavbarComponent, RegistrationComponent, ResetPasswordComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
