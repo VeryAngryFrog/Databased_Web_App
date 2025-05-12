cb768286858c61484414b2a80f589fbdeffe4104
PaselCobra <sbrockhoff@stud.hs-bremen.de>
2025-05-12 12:48:09 +0200

    Simon
    @Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+<<<<<<< Updated upstream
 import {NavbarComponent} from "./navbar/navbar.component";
 
 
 @Component({
     selector: 'app-root',
     imports: [RouterOutlet, NavbarComponent],
+=======
+import { LoginComponent } from './login/login.component';
+
+@Component({
+    selector: 'app-root',
+    imports: [RouterOutlet, LoginComponent],
+>>>>>>> Stashed changes
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
