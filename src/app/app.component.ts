e9f47dc14ffdedd94417b6f33e91941d41f6a5b9
TBY01Z <rieckersniclas@gmail.com>
2025-05-12 12:53:16 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< Updated upstream
+import { LoginComponent } from './login/login.component';
 import {NavbarComponent} from "./navbar/navbar.component";
 
-
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, NavbarComponent],
-=======
-import { LoginComponent } from './login/login.component';
-
-@Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, LoginComponent],
->>>>>>> Stashed changes
+    imports: [RouterOutlet, LoginComponent, NavbarComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
