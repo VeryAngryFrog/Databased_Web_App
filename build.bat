1fb797c424514295ce83ebf51b74aa82e68f7546
TBY01Z <rieckersniclas@gmail.com>
2025-06-02 11:52:30 +0200

    added batch

---
+++ build.bat
@@ -0,0 +1,5 @@
+ng build
+sleep 20
+node C:\Users\HP\OneDrive\New_Strukture\Schule\HS-Bremen\Datenbankbasierte Webanwendung\Databased_Web_App\dist\databased-web-app\server\server.mjs
+sleep 20
+start http:\\localhost:4000
